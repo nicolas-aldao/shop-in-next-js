8c91bc28c9eff752ceed859f646d45a56e78ffcb
Nicolas Aldao <nicolasaldao@Nicolass-MacBook-Pro.local>
2023-01-26 20:29:37 -0300

    some changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+// import { Script } from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet='utf-8' />
         {/* <script async src=""></script>
-        <script dangerouslySetInnerHTML={ __html: ``}/> */}
+        <Script dangerouslySetInnerHTML={ __html: ``}/> */}
       </Head>
       <body>
         <Head />
